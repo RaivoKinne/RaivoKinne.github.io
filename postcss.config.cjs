287c2123818bbb22e441dbbad1aef24854041175
RaivoKinne <raivokinne236@gmail.com>
2023-03-10 17:35:46 +0200

    postcss commit

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-const postcssPresetEnv = require("postcss-preset-env")
-const autoprefixer = require("autoprefixer")
-
-module.exports = {
-  plugins: [
-    autoprefixer(),
-    postcssPresetEnv({stage:1})
-  ]
-}
