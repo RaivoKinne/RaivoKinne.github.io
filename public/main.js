a977a9766b67e16dd2e1c2274144d538f3c49965
RaivoKinne <raivokinne236@gmail.com>
2023-03-11 08:26:13 +0200

    comit

--- public/main.js
+++ public/main.js
@@ -1,15 +1,22 @@
 const button = document.querySelector(".hamburger-icon")
 const menu = document.querySelector(".menu")
+const contactButton = document.querySelector(".contact-button")
 
 button.addEventListener("click", () => {
     const isOpened = button.getAttribute("aria-expanded")
     const visibility = menu.getAttribute("data-visible")
+    const closed = contactButton.getAttribute("data-visible")
     
-    if(isOpened === "false" || visibility === "false"){
+    if(isOpened === "false" && visibility === "false" && closed === "false"){
         button.setAttribute("aria-expanded","true")
         menu.setAttribute("data-visible",true)
+        contactButton.setAttribute("data-visible", true);
     } else {
         button.setAttribute("aria-expanded", "false")
         menu.setAttribute("data-visible", false);
+        setTimeout(()=> {
+            contactButton.setAttribute("data-visible", false);
+        },800)
+        
     }
 })
